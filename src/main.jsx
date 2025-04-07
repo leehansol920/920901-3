215fca635c13ecf79e385a841c1cf74404204834
leehansol920 <jinmoa9757@nate.com>
2025-04-07 16:19:53 +0900

    Add files via upload

---
+++ src/main.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+function App() {
+  return <h1>더튜브 공장관리앱</h1>;
+}
+ReactDOM.createRoot(document.getElementById('root')).render(<App />);
